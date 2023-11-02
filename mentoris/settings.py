c69fda107138cb833df2082b38712786fbcfee54
janiceparkk <jparkowl@gmail.com>
2023-11-02 09:24:47 -0700

    Merging Base Feedback w/ Main

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -81,14 +81,6 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "mentoris",
-        "USER": "jpark",
-        "PASSWORD": "pass4Janice",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
 }
 
 
