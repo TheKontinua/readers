ac1cc486a03107770d92bae70e79a3470ca06d32
Aaron Bettencourt <aabetten@calpoly.edu>
2023-11-06 17:14:27 -0800

    Configured for elastic beanstalk

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pbmmo011$5_b^69e9k-#1#)i8)*!n)io8-y0rz=@&e6@h7s!zk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     'mentoris/static'
 ]
