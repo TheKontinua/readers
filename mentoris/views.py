149a3c06761d2afd7598f88524b94d809849bbda
Noah Ravetch <nravetch@calpoly.edu>
2023-12-10 21:52:30 -0800

    Fixed login page

--- mentoris/views.py
+++ mentoris/views.py
@@ -88,7 +88,7 @@ def profile(request):
 def login(request):
     if request.method == "POST":
         email = request.POST.get("email")
-        password = request.POST.get("password_hash")
+        password = request.POST.get("password")
 
         try:
             emailObject = Email.objects.get(email_address=email)
