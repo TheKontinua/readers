2eb3b9fd2c3a6a7b8a901860fba12a765a3cce80
aabettschool <123058171+aabettschool@users.noreply.github.com>
2024-01-27 17:11:55 -0800

    misc change

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -75,16 +75,7 @@ WSGI_APPLICATION = "mentoris.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'MentappMentorispasSCars2ElasticStalkBean!',
-        'HOST': 'mentorisdb.cmkrvc9icttm.us-west-2.rds.amazonaws.com',
-        'PORT': '5432',
-        }
-}
+
 
 
 STORAGES = {
