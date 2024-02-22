2fb46dc03216da7c0b2e4c1c7e7ad59b32a19acf
HarisKhan0 <71156152+HarisKhan0@users.noreply.github.com>
2024-02-22 09:32:31 -0800

    Removed Database

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -75,17 +75,6 @@ WSGI_APPLICATION = "mentoris.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mentoris',
-        'USER': 'walden',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 
 STORAGES = {
     "default": {
