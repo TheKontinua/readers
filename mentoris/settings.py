81cbb7eefcee536eff35aa9faccf5f96e9eb2093
ashton14 <ashtonalonge14@gmail.com>
2024-02-26 21:08:16 -0800

    db

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -78,23 +78,7 @@ WSGI_APPLICATION = "mentoris.wsgi.application"
 
 DATABASES = {     
 
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'postgres',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'MentappMentorispasSCars2ElasticStalkBean!',
-    #     'HOST': 'mentorisdb.cmkrvc9icttm.us-west-2.rds.amazonaws.com',
-    #     'PORT': '5432',
-    #     }
-
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mentoris',
-        'USER': 'ashton',
-        'PASSWORD': 'postgreshton',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        }
+    
         
     # "default": {
     #      "ENGINE": "django.db.backends.sqlite3",
