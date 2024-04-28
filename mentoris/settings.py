5445009014852b8166eb8634f92af259f1fd7445
aabettschool <123058171+aabettschool@users.noreply.github.com>
2024-04-27 23:09:42 -0700

    Fixed template not found bug on AWS

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.conf import settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,7 +62,7 @@ TEMPLATES = [
         "DIRS": [
             "mentoris/templates",
             "mentapp/templates,",
-            "mentoris/mentoris/templates/mentapp",
+            os.path.join(settings.BASE_DIR, "/mentoris/templates/mentapp"),
         ],
         "APP_DIRS": True,
         "OPTIONS": {
