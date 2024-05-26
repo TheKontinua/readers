a943d23ba203717ed32d04b7bd91f4a277744ef8
aabettschool <123058171+aabettschool@users.noreply.github.com>
2024-05-26 14:35:21 -0700

    LaTeX macro ready, times out when saving due to pdf generation

--- mentoris/settings.py
+++ mentoris/settings.py
@@ -85,7 +85,13 @@ WSGI_APPLICATION = "mentoris.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'helloworld',
+    }
+}
+
 
 
 STORAGES = {
