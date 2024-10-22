293097d5cd42a248f29771f8207be11918b7b851
Devin Hadley <devinhadley@Devins-MacBook-Air.local>
2024-10-22 08:40:15 -0700

    feat: pdf view retrieves from local server

---
+++ ReaderIOS/ReaderIOSTests/ReaderIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReaderIOSTests.swift
+//  ReaderIOSTests
+//
+//  Created by Devin Hadley on 10/22/24.
+//
+
+import Testing
+@testable import ReaderIOS
+
+struct ReaderIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
