1428d9df49bd4c13d33002e51667fe118c763e09
Devin Hadley <devinhadley@Devins-Air-2.lan>
2024-10-24 10:25:22 -0700

    feat: upload all workbooks from static

--- ReaderIOS/ReaderIOS/ReaderIOSApp.swift
+++ ReaderIOS/ReaderIOS/ReaderIOSApp.swift
@@ -15,3 +15,7 @@ struct ReaderIOSApp: App {
         }
     }
 }
+
+#Preview {
+    PDFView()
+}
--- ReaderIOS/ReaderIOSUITests/ReaderIOSUITests.swift
+++ ReaderIOS/ReaderIOSUITests/ReaderIOSUITests.swift
@@ -29,6 +29,7 @@ final class ReaderIOSUITests: XCTestCase {
         app.launch()
 
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
     }
 
     @MainActor
