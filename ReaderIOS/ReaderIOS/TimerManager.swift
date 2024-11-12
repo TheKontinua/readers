67b533cb33b0e9319f315e0d5b88f8f99ea5f776
mollysandler <74861421+mollysandler@users.noreply.github.com>
2024-11-12 09:44:28 -0800

    refactor timer to its own file, remove padding

---
+++ ReaderIOS/ReaderIOS/TimerManager.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+
+class TimerManager: ObservableObject {
+    @Published var selectedDuration: TimeInterval = 0
+    @Published var progress: Double = 0
+    @Published var isTimerRunning: Bool = false
+    @Published var remainingDuration: TimeInterval = 0
+    @Published var isPaused: Bool = false
+    
+    private var timer: Timer?
+    
+    func startTimer(duration: TimeInterval) {
+        selectedDuration = duration
+        progress = 0
+        remainingDuration = duration
+        timer?.invalidate()
+        isTimerRunning = true
+        isPaused = false
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.progress += 1 / duration
+            self.remainingDuration -= 1
+            if self.remainingDuration <= 0 {
+                self.timer?.invalidate()
+                self.timer = nil
+                self.isTimerRunning = false
+                self.progress = 1
+            }
+        }
+    }
+    
+    func pauseTimer() {
+        timer?.invalidate()
+        timer = nil
+        isTimerRunning = false
+        isPaused = true
+    }
+    
+    func unpauseTimer() {
+        guard !isTimerRunning else { return }
+        
+        isTimerRunning = true
+        isPaused = false
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            self.progress += 1 / self.selectedDuration
+            self.remainingDuration -= 1
+            if self.remainingDuration <= 0 {
+                self.timer?.invalidate()
+                self.timer = nil
+                self.isTimerRunning = false
+            }
+        }
+    }
+    
+    func restartTimer() {
+        startTimer(duration: selectedDuration)
+    }
+    
+    func cancelTimer() {
+        timer?.invalidate()
+        timer = nil
+        progress = 0
+        remainingDuration = 0
+        isTimerRunning = false
+        isPaused = false
+    }
+}
